c6d6fd8dd63eeaf8bd08388be7878f1db53d78be
cyberhearts <untelentedmind@gmail.com>
2023-03-10 20:13:16 +0600

    [🧪] Write tests for posts model and creating user

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+
+class BlogTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email='test@gmail.com',
+            password = 'testpass'
+        )
+
+        self.post = Post.objects.create(
+            title = 'Title',
+            body = 'Body',
+            author = self.user,
+        )
+
+    def test_string_representation(self):
+        post = Post(title="Title test")
+        self.assertEqual(str(post), post.title)
+    
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'Title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Body')
+    
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Body')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Title')
+        self.assertTemplateUsed(response, 'postpage.html')
+
